06e4b6f098f4d35f98f5a45be7a56ca88dce1772
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2024-05-08 23:32:11 -0400

    added alt text to img

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
     return(
         <div className="px-[100px] py-[30px] flex items-center justify-between shadow-3xl">
             <div className="flex flex-1 items-center">
-                <Image src={Logo} width={25}/>
+                <Image src={Logo} width={25} alt="logo"/>
                 <span className="ml-[10px]">KeepITSimple</span>
             </div>
             <div className="flex-1 text-center">
